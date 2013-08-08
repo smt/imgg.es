28d18b285b6e793e9e6f6a10375f44238615781c
Stephen Tudor <smtudor@gmail.com>
2013-08-07 20:06:31 -0400

    Spaces, you fools

--- test/sites.js
+++ test/sites.js
@@ -1,56 +1,56 @@
- var request = require('request'),
- 	 should = require('should');
+var request = require('request'),
+should = require('should');
 
- describe('Sites are accessible', function() {
- 	it('bukk.it should be online', function() {
- 		request('http://bukk.it/', function(error, result, body) {
- 			if (error) {
- 				console.log(error); 				
- 				return false;
- 			}
- 			else {
- 				result.statusCode.should.be.equal(200);
- 				should.exist(body);
- 			}
- 		});
- 	});
+describe('Sites are accessible', function() {
+    it('bukk.it should be online', function() {
+        request('http://bukk.it/', function(error, result, body) {
+            if (error) {
+                console.log(error);
+                return false;
+            }
+            else {
+                result.statusCode.should.be.equal(200);
+                should.exist(body);
+            }
+        });
+    });
 
- 	it('wil.to should be online', function() {
- 		request('http://wil.to/_/', function(error, result, body) {
- 			if (error) {
- 				console.log(error);
- 				return false;
- 			}
- 			else {
- 				result.statusCode.should.be.equal(200);
- 				should.exist(body);
- 			}
- 		});
- 	});
+    it('wil.to should be online', function() {
+        request('http://wil.to/_/', function(error, result, body) {
+            if (error) {
+                console.log(error);
+                return false;
+            }
+            else {
+                result.statusCode.should.be.equal(200);
+                should.exist(body);
+            }
+        });
+    });
 
- 	it('misatkes should be online', function() {
- 		request('http://misatkes.com/', function(error, result, body) {
- 			if (error) {
- 				console.log(error); 				
- 				return false;
- 			}
- 			else {
- 				result.statusCode.should.be.equal(200);
- 				should.exist(body);
- 			}
- 		});
- 	});
+    it('misatkes should be online', function() {
+        request('http://misatkes.com/', function(error, result, body) {
+            if (error) {
+                console.log(error);
+                return false;
+            }
+            else {
+                result.statusCode.should.be.equal(200);
+                should.exist(body);
+            }
+        });
+    });
 
- 	it('meyerweb should be online', function() {
- 		request('http://meyerweb.com/bkkt/', function(error, result, body) {
- 			if (error) {
- 				console.log(error); 				
- 				return false;
- 			}
- 			else {
- 				result.statusCode.should.be.equal(200);
- 				should.exist(body);
- 			}
- 		});
- 	});
- });
+    it('meyerweb should be online', function() {
+        request('http://meyerweb.com/bkkt/', function(error, result, body) {
+            if (error) {
+                console.log(error);
+                return false;
+            }
+            else {
+                result.statusCode.should.be.equal(200);
+                should.exist(body);
+            }
+        });
+    });
+});
