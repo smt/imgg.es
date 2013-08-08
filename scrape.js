2a7e1164ec18de40326c918d38e4fd2f2998b969
Stephen Tudor <smtudor@gmail.com>
2013-08-07 20:06:09 -0400

    First pass at a refactor of scrape.js

--- scrape.js
+++ scrape.js
@@ -5,50 +5,75 @@ var execTime = require('exec-time')
 var fs = require('fs');
 var request = require('request');
 
+var sites = [
+    'http://bukk.it',
+    'http://wil.to/_',
+    'http://misatkes.com',
+    'http://meyerweb.com/bkkt'
+];
+
 var profiler = new execTime('processing sites');
+
+function compareItems(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+}
+
+function formatResults(results) {
+    var sortedList, i = 1, len = results.length, args = [];
+    for (; i < len; i++) {
+        args.push(results[i]);
+    }
+    sortedList = Array.prototype.concat.apply(results[0], args).sort(compareItems);
+    return sortedList;
+}
+
+function outputJson(images) {
+    var serializedStr = JSON.stringify(images);
+    fs.writeFile('images.json', serializedStr);
+    profiler.step('done');
+    return serializedStr;
+}
+
+function parallelDone(err, results) {
+    if (err || !results || !results.length) return console.log('async.parallel: %s - %s', err, results);
+    var images;
+    images = formatResults(results);
+    console.log('Processed ' + images.length + ' images');
+    return outputJson(images);
+}
+
+function mapDone(err, results) {
+    if (err || !results || !results.length) return console.log('async.map: %s - %s', err, results);
+    return async.parallel(results, parallelDone);
+}
+
+function createLink(origin, name) {
+    var url = origin + '/' + name;
+    return {
+        id: crypto.createHash('md5').update(url).digest('hex'),
+        url: url,
+        name: name,
+        origin: origin
+    };
+}
+
+function eachOrigin(origin, callback) {
+    return callback(null, function (parallelCallback) {
+        return request(origin, function (err, res, body) {
+            var $ = cheerio.load(body);
+            var selector = 'a[href$=".gif"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"]';
+            var links = [];
+            $(selector).each(function () {
+                var link = createLink(origin, $(this).attr('href'));
+                links.push(link);
+            });
+            return parallelCallback(null, links);
+        });
+    });
+}
 
 profiler.beginProfiling();
 
-async.map(['http://bukk.it', 'http://wil.to/_/', 'http://misatkes.com', 'http://meyerweb.com/bkkt/'],
-    function (origin, mapCallback) {
-        mapCallback(null, function (seriesCallback) {
-            request(origin, function (err, res, body) {
-                var $ = cheerio.load(body);
-                var selector = 'a[href$=".gif"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"]';
-                var links = [];
-                $(selector).each(function () {
-                    var name = $(this).attr('href');
-                    var url = origin + '/' + name;
-                    links.push({
-                        id: crypto.createHash('md5').update(url).digest('hex'),
-                        url: url,
-                        name: name,
-                        origin: origin
-                    });
-                });
-                seriesCallback(null, links);
-            });
-        });
-    },
-    function (err, results) {
-        if (err || !results || !results.length) return console.log('async.map: %s - %s', err, results);
-        var images;
-        async.parallel(results, function (err, results) {
-            if (err || !results || !results.length) return console.log('async.series: %s - %s', err, results);
-            var i, len, args = [];
-            for (i = 1, len = results.length; i < len; i++) {
-                args.push(results[i]);
-            }
-            images = Array.prototype.concat.apply(results[0], args).sort(function (a, b) {
-                if (a.name < b.name) return -1;
-                if (a.name > b.name) return 1;
-                return 0;
-            });
-            console.log(images);
-            console.log('Processed ' + images.length + ' images');
-            images = JSON.stringify(images);
-            fs.writeFile('images.json', images);
-            profiler.step('done');
-        });
-    }
-);
+async.map(sites, eachOrigin, mapDone);
