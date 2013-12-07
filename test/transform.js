7a5329b90b5471e00960441c925654750d5b30c1
Stephen Tudor <smtudor@gmail.com>
2013-12-07 08:14:52 -0800

    Fix transform test

--- test/transform.js
+++ test/transform.js
@@ -4,16 +4,13 @@ var transform = require('../lib/transform').transform;
 
 describe('transform', function () {
     it('should yield a correctly formatted link object', function () {
-        // TODO: this test currently fails due to something related to the
-        // regex replace being used in the transform method
-
-        // transform('http://wil.to/_', 'loremipsum.jpg').should.eql({
-        //     url: 'http://cdn.imgg.es/wil.to__loremipsum.jpg',
-        //     data: {
-        //         domain: 'wil.to/_',
-        //         name: 'loremipsum.jpg',
-        //         origUrl: 'http://wil.to/_/loremipsum.jpg',
-        //     }
-        // });
+        transform('http://wil.to/_', 'loremipsum.jpg').should.eql({
+            url: 'http://cdn.imgg.es/wil.to___loremipsum.jpg',
+            data: {
+                domain: 'wil.to/_',
+                name: 'loremipsum.jpg',
+                origUrl: 'http://wil.to/_/loremipsum.jpg',
+            }
+        });
     });
 });
